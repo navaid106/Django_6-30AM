a63832ba60094c37d42a6af37a5f9a2c41679077
mohd navaid <mailtonavaid@gmail.com>
2025-09-20 09:56:31 -0400

    Day-35 adding login functionality...

--- Day-34/authproject/authproject/urls.py
+++ Day-34/authproject/authproject/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from testapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home_page_view),
+    path('java/',views.java_page_view),
+    path('python/',views.python_page_view),
+    path('aptitude/',views.aptitude_page_view),
+    path('accounts/',include('django.contrib.auth.urls')),
 ]
