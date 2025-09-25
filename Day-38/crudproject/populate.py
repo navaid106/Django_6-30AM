186749d9ee8564db76435902b8ccb4c0b536261a
mohd navaid <mailtonavaid@gmail.com>
2025-09-24 20:49:01 -0400

    DAY-39 | CRUD OPERATIONS DONE

---
+++ Day-38/crudproject/populate.py
@@ -0,0 +1,33 @@
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crudproject.settings')
+
+import django
+django.setup()
+
+from testapp.models import Employee
+
+from faker import Faker
+from random import *
+faker = Faker()
+
+def populate(n): #n means no. of records
+    for i in range(n):
+        feno = randint(1001,9999)
+        fename = faker.name()
+        fesal = randint(10000,20000)
+        feaddr = faker.city()
+
+        emp_record =  Employee.objects.get_or_create(
+            eno = feno,
+            ename = fename,
+            esal = fesal,
+            eaddr = feaddr 
+
+        )
+
+n = int(input('Enter number of employees: '))
+populate(n)
+
+print(f"{n}, Records inserted successfully....")
+
